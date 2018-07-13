5b6aba45648c66badfe1b3131f44d7a571ccf45f
eckmLJE <lucas.eckman@gmail.com>
2018-07-13 09:28:52 -0400

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import { addRestaurant } from '../actions/restaurants';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { addRestaurant } from "../actions/restaurants";
+import { connect } from "react-redux";
+import { bindActionCreators } from "../../node_modules/redux";
 
 export class RestaurantInput extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      name: '', location: ''
+      name: "",
+      location: ""
     };
   }
 
@@ -30,24 +31,39 @@ export class RestaurantInput extends Component {
   }
 
   render() {
-    return(
-      <form onSubmit={(event) => this.handleOnSubmit(event)}>
+    return (
+      <form onSubmit={event => this.handleOnSubmit(event)}>
         <p>
-          <input 
-            type="text" 
-            onChange={(event) => this.handleOnNameChange(event)} 
-            placeholder="restaurant name" />
+          <input
+            type="text"
+            onChange={event => this.handleOnNameChange(event)}
+            placeholder="restaurant name"
+          />
         </p>
         <p>
-          <input 
-            type="text" 
-            onChange={(event) => this.handleOnLocationChange(event)} 
-            placeholder="location" />
+          <input
+            type="text"
+            onChange={event => this.handleOnLocationChange(event)}
+            placeholder="location"
+          />
         </p>
         <input type="submit" />
       </form>
     );
   }
+}
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ addRestaurant: addRestaurant }, dispatch);
 };
 
-export const ConnectedRestaurantInput = connect(null, null)(RestaurantInput)
+const mapStateToProps = state => {
+  return {
+    restaurants: state.restaurants
+  };
+};
+
+export const ConnectedRestaurantInput = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RestaurantInput);
